fc6457f2200ef23e00ec19844f0b647b33777eab
Masaya-Takada <masaya.takada22@gmail.com>
2024-04-08 08:25:45 +0900

    ヘッダーコンポーネントの機能を最低限実装

---
+++ app/header.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import Link from 'next/link';
+
+const PATHS: { href: string, label: string }[] = [
+  { href: 'about', label: 'about' },
+  { href: 'post', label: 'blog' },
+]
+
+const LINKS = [
+  { icon: <FaXTwitter />, href: 'https://twitter.com/takada_masaya_' },
+  { icon: <FaGithub />, href: 'https://github.com/TakadaMasaya' },
+]
+
+export default function Header() {
+  return (
+    <Navbar>
+      <NavbarBrand>たかだ</NavbarBrand>
+      <NavbarContent justify="start" className='hidden sm:flex gap-4'>
+        {PATHS.map(({ href, label }) =>
+          <NavbarItem key={href}>
+            <Link href={href} target='_blank'>{label}</Link>
+          </NavbarItem>
+        )}
+      </NavbarContent>
+
+      <NavbarContent justify="end" className='hidden sm:flex gap-4'>
+        {LINKS.map(({ href, icon }) =>
+          <NavbarItem key={href}>
+            <Link href={href} target='_blank'>{icon}</Link>
+          </NavbarItem>
+        )}
+      </NavbarContent>
+
+    </Navbar>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UiProvider } from "./UiProvider";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <UiProvider>
+          <Header />
           {children}
         </UiProvider>
       </body>
