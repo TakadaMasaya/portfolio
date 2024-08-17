3ea5445204c63d31addf557011a8debc42963c82
Masaya-Takada <masaya.takada22@gmail.com>
2024-08-17 17:52:30 +0900

    feat: #1 ヘッダー部分の実装が一旦完了

--- app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function About() {
-  return <div className='text-6xl text-pink-100'>
-    this is a page about me
-  </div>
-}
--- app/header.tsx
+++ app/header.tsx
@@ -1,62 +1,45 @@
-'use client'
-
-import React from 'react'
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
-import { TbMenu2 as MenuHamburger } from "react-icons/tb";
-import Link from 'next/link';
-
-const PATHS: { href: string, label: string }[] = [
-  { href: 'about', label: 'about' },
-  { href: 'blog', label: 'blog' },
-]
+import { Image, Link } from '@nextui-org/react'
+import { FaGithub } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+import { SiZenn } from 'react-icons/si'
 
 const LINKS = [
-  { icon: <FaXTwitter />, href: 'https://twitter.com/takada_masaya_' },
-  { icon: <FaGithub />, href: 'https://github.com/TakadaMasaya' },
+  {
+    icon: <FaGithub className="h-full w-full" />,
+    href: 'https://github.com/TakadaMasaya',
+  },
+  {
+    icon: <FaXTwitter className="h-full w-full" />,
+    href: 'https://twitter.com/takada_masaya_',
+  },
+  {
+    icon: <SiZenn className="h-full w-full" />,
+    href: 'https://zenn.dev/takada',
+  },
 ]
 
 export default function Header() {
   return (
-    <Navbar>
-      <NavbarBrand>
-        <Link href='/'>
-          たかだ
-        </Link>
-      </NavbarBrand>
-      <NavbarContent justify="start" className='hidden sm:flex gap-4'>
-        {PATHS.map(({ href, label }) =>
-          <NavbarItem key={href}>
-            <Link href={href}>{label}</Link>
-          </NavbarItem>
-        )}
-      </NavbarContent>
-
-      <NavbarContent justify="end" className='hidden sm:flex gap-4'>
-        {LINKS.map(({ href, icon }) =>
-          <NavbarItem key={href}>
-            <Link href={href} target='_blank'>{icon}</Link>
-          </NavbarItem>
-        )}
-      </NavbarContent>
-
-      <NavbarContent justify='end' className='sm:hidden'>
-        <Dropdown>
-          <DropdownTrigger>
-            <Button isIconOnly>
-              <MenuHamburger fontSize={24} />
-            </Button>
-          </DropdownTrigger>
-          <DropdownMenu aria-label='header menu'>
-            {PATHS.map(({ href, label }) =>
-              <DropdownItem key={href} href={href}>
-                {label}
-              </DropdownItem>
-            )}
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-    </Navbar>
+    <header className="w-full max-w-2xl bg-background text-foreground dark">
+      <div className="flex justify-between pb-3 pt-10 align-bottom">
+        <div className="flex flex-col justify-center gap-2">
+          <h1 className="text-2xl font-bold">たかだ まさや</h1>
+          <div className="flex gap-2">
+            {LINKS.map(({ icon, href }) => (
+              <Link key={href} href={href} target="_blank" className="size-5">
+                {icon}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <Image src="/portrait.jpg" alt="portrait" className="w-24" />
+      </div>
+      <div>
+        <p>
+          Webフロントエンドエンジニアです。業務の自動化や効率化に興味があります。
+        </p>
+        <p>普段はReactやTypeScriptを用いた開発をしています。</p>
+      </div>
+    </header>
   )
 }
