881a44ff109f558fdb9fe2edc39a2e237ea5ce1c
Masaya-Takada <masaya.takada22@gmail.com>
2024-08-17 21:52:33 +0900

    feat: #9 タブの切り替え時にページのロードが発生していたのを修正

--- app/contentTabs.tsx
+++ app/contentTabs.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { Tab, Tabs } from '@nextui-org/react'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
+import { Key } from 'react'
 
 const PATHS: { href: string; title: string }[] = [
   { href: '/skills', title: 'できること' },
@@ -10,15 +11,22 @@ const PATHS: { href: string; title: string }[] = [
 
 export default function ContentTabs() {
   const pathname = usePathname()
+  const router = useRouter()
+
+  const handleChange = (href: Key) => {
+    if (typeof href !== 'string') throw Error('keyがhref属性ではありません')
+    router.push(href)
+  }
 
   return (
     <Tabs
       variant="underlined"
       aria-label="page routing tab"
       selectedKey={pathname}
+      onSelectionChange={(key) => handleChange(key)}
     >
       {PATHS.map(({ href, title }) => (
-        <Tab key={href} title={title} href={href} />
+        <Tab key={href} title={title} />
       ))}
     </Tabs>
   )
