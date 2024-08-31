2974624a83c3ec14ae42a0eba3ba3d3f5cdbadee
takada-masaya <masaya.takada22@gmail.com>
2024-08-31 19:56:13 +0900

    feat: 経歴ページのコンテンツを日付順の降順になるよう変更し、日付をサブタイトルとして表示

--- app/career/careerWrapper.tsx
+++ app/career/careerWrapper.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { CAREER_START_ICON } from './careerAccordionItemStartIcon'
-import { CareerContents } from './page'
+import { CareerContent, CareerContents } from './page'
 
 import { Accordion, AccordionItem } from '@nextui-org/react'
+import { CareerType } from './types/careerTypes'
 
 type PropTypes = {
   contents: CareerContents
@@ -12,23 +13,47 @@ type PropTypes = {
 export default function CareerWrapper({ contents }: PropTypes) {
   return (
     <Accordion variant="splitted">
-      {contents.map((careerObj) => {
-        const { id, title, content, careerType } = careerObj
-        return (
+      {contents
+        .sort((a, b) => (a.startDate > b.startDate ? -1 : 1))
+        .map(({ id, title, content, careerType, startDate, endDate, isActive }) => (
           <AccordionItem
             key={id}
             title={<div className="text-base">{title}</div>}
-            startContent={
-              <div className="text-primary">{CAREER_START_ICON.get(careerType[0])}</div>
-            }
+            subtitle={getSubTitle(startDate, endDate, isActive)}
+            startContent={getStartContent(careerType[0])}
           >
             <div
               dangerouslySetInnerHTML={{ __html: content }}
-              className='[&_p]:indent-4 [&_ul]:pl-8 [&_ul]:list-disc' 
+              className="[&_p]:indent-4 [&_ul]:list-disc [&_ul]:pl-8"
             />
           </AccordionItem>
-        )
-      })}
+        ))}
     </Accordion>
   )
+}
+
+const getStartContent = (careerType: CareerType) => (
+  <div className="text-primary">{CAREER_START_ICON.get(careerType[0])}</div>
+)
+
+const getYearMonth = (dateStr: string) => {
+  const date = new Date(dateStr)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+
+  return `${year}年${month}月`
+}
+
+const getSubTitle = (
+  start: CareerContent['startDate'],
+  end: CareerContent['endDate'],
+  isActive: CareerContent['isActive'],
+) => {
+  let subTitle: string = ''
+
+  subTitle = getYearMonth(start)
+  if (isActive) subTitle += ' - 現在'
+  else if (end) subTitle += ` - ${getYearMonth(end)}`
+
+  return subTitle
 }
